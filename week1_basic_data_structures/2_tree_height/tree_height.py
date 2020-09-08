46f6b06e3eade4e6dd1427394c4d0aa4ec94b084
Ghilman AL Fatih <ghilman27@gmail.com>
2020-09-08 17:09:17 +0700

    week 1: problem 2 submit

--- week1_basic_data_structures/2_tree_height/tree_height.py
+++ week1_basic_data_structures/2_tree_height/tree_height.py
@@ -2,19 +2,85 @@
 
 import sys
 import threading
+from collections import deque
+
+
+class Node:
+    def __init__(self, parent=None, children=None):
+        self.parent = parent
+        self.children = []
+        if children:
+            self.children.extend(children)
+    
+    def add_child(self, child):
+        self.children.append(child)
+    
+    def set_parent(self, parent):
+        self.parent = parent
+
+
+class Tree:
+    def __init__(self, root=None, nodes=None):
+        self.root = root
+        self.nodes = []
+        if nodes:
+            self.nodes.extend(nodes)
+
+    def set_root(self, root):
+        self.root = root
+    
+    def add_node(self, node):
+        self.nodes.append(node)
+    
+    def compute_height(self):
+        if len(self.nodes) == 0:
+            return 0
+        
+        queue = deque()
+        queue.append(self.root)
+        height = 0
+        level_nodes = 1
+        while len(queue) > 0:
+            node = queue.popleft()
+            level_nodes -= 1
+            for child in node.children:
+                queue.append(child)
+            if level_nodes == 0:
+                height += 1
+                level_nodes = len(queue)
+        
+        return height
+
+
+def allocate_nodes(parents):
+    nodes = []
+    for parent in parents:
+        nodes.append(Node())
+    return nodes
+
+
+def link_nodes(n, nodes, parents):
+    root_node_idx = -1
+    if n != len(nodes) or n != len(parents):
+        raise Exception('Please input the same number of nodes and parents')
+
+    for child_idx, parent_idx in enumerate(parents):
+        if parent_idx == -1:
+            root_node_idx = nodes[child_idx]
+        else:
+            child_node = nodes[child_idx]
+            parent_node = nodes[parent_idx]
+            child_node.set_parent(parent_node)
+            parent_node.add_child(child_node)
+    
+    return root_node_idx, nodes
 
 
 def compute_height(n, parents):
-    # Replace this code with a faster implementation
-    max_height = 0
-    for vertex in range(n):
-        height = 0
-        current = vertex
-        while current != -1:
-            height += 1
-            current = parents[current]
-        max_height = max(max_height, height)
-    return max_height
+    nodes = allocate_nodes(parents)
+    root_node, nodes = link_nodes(n, nodes, parents)
+    tree = Tree(root=root_node, nodes=nodes)
+    return tree.compute_height()
 
 
 def main():
@@ -23,9 +89,6 @@ def main():
     print(compute_height(n, parents))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+if __name__ == '__main__':
+    main()
+
