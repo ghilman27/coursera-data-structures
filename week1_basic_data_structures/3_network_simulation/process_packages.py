90d5a3506ac311e68412f2117a15bc37ac967087
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-08 21:37:03 +0700

    week 1: problem 3 submit

--- week1_basic_data_structures/3_network_simulation/process_packages.py
+++ week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -1,6 +1,6 @@
 # python3
 
-from collections import namedtuple
+from collections import namedtuple, deque
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
@@ -9,11 +9,22 @@ Response = namedtuple("Response", ["was_dropped", "started_at"])
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish_time = []
+        self.finish_time = deque()
 
     def process(self, request):
         # write your code here
-        return Response(False, -1)
+        # 1. Check finished processes
+        while len(self.finish_time) > 0 and self.finish_time[0] <= request.arrived_at:
+            self.finish_time.popleft()
+
+        # 2. Check buffer availability
+        start_time = request.arrived_at if len(self.finish_time) == 0 else self.finish_time[-1]
+        if len(self.finish_time) < self.size:
+            finish_time = start_time + request.time_to_process
+            self.finish_time.append(finish_time)
+            return Response(was_dropped=False, started_at=start_time)
+        
+        return Response(was_dropped=True, started_at=start_time)
 
 
 def process_requests(requests, buffer):
