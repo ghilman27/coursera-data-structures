41557252dd2cd007b8b7ee71cd2ce551dffe9bcf
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-08 22:00:13 +0700

    week 1: problem 4 submit

--- week1_basic_data_structures/4_stack_with_max/stack_with_max.py
+++ week1_basic_data_structures/4_stack_with_max/stack_with_max.py
@@ -1,20 +1,26 @@
 #python3
 import sys
+from collections import deque
 
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.max = deque([float('-inf')])
 
     def Push(self, a):
         self.__stack.append(a)
+        if a >= self.max[-1]:
+            self.max.append(a)
 
     def Pop(self):
         assert(len(self.__stack))
-        self.__stack.pop()
+        popped_value = self.__stack.pop()
+        if popped_value == self.max[-1]:
+            self.max.pop()
 
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return self.max[-1]
 
 
 if __name__ == '__main__':
