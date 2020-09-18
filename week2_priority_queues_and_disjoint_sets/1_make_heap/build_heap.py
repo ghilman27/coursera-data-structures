2d99f7edba3f0f0f689d74f4e31062e530bb440e
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-18 17:40:27 +0700

    week 2: problem 1 submit

--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,24 +1,49 @@
 # python3
 
 
+class MinHeap:
+    def __init__(self, data):
+        self.data = data
+        self.size = len(data)
+
+    
+    def shift_down(self, index, return_swaps=False):
+        swaps = []
+        max_index = index
+        while True:
+            left_index = 2*index + 1
+            right_index = 2*index + 2
+            if left_index < self.size and self.data[left_index] < self.data[max_index]:
+                max_index = left_index
+            
+            if right_index < self.size and self.data[right_index] < self.data[max_index]:
+                max_index = right_index
+            
+            if index != max_index:
+                swaps.append((index, max_index))
+                self.data[index], self.data[max_index] = self.data[max_index], self.data[index]
+                index = max_index
+            else:
+                break
+        
+        if return_swaps:
+            return swaps
+
+    
+    def build_heap(self):
+        swaps = []
+        for index in range((self.size-1) // 2, -1, -1):
+            swaps.extend(self.shift_down(index, return_swaps=True))
+        return swaps
+        
+
 def build_heap(data):
     """Build a heap from ``data`` inplace.
 
     Returns a sequence of swaps performed by the algorithm.
     """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    min_heap = MinHeap(data)
+    return min_heap.build_heap()
 
 
 def main():
