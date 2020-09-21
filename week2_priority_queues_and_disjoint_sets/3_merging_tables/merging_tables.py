82d1650d6c3bcff08e7a1add22d7bcf0f111e4b2
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-22 00:30:41 +0700

    week 2: problem 3 submit

--- week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -19,11 +19,40 @@ class Database:
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        src_parent = self.get_parent(src)
+        dst_parent = self.get_parent(dst)
+        if src_parent == dst_parent:
+            return False
+
+        if self.ranks[dst_parent] > self.ranks[src_parent]:
+            self.parents[src_parent] = dst_parent
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+            final_rows = self.row_counts[dst_parent]
+        else:
+            self.parents[dst_parent] = src_parent
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+            if self.ranks[dst_parent] == self.ranks[src_parent]:
+                self.ranks[src_parent] += 1
+            final_rows = self.row_counts[src_parent]
+        
+        self.max_row_count = max(final_rows, self.max_row_count)
         return True
 
     def get_parent(self, table):
         # find parent and compress path
-        return self.parents[table]
+        paths = []
+        while True:
+            if table != self.parents[table]:
+                paths.append(table)
+                table = self.parents[table]
+            else:
+                break
+        
+        root_table = table
+        if len(paths) != 0:
+            for table in paths:
+                self.parents[table] = root_table
+        return root_table
 
 
 def main():
@@ -31,7 +60,7 @@ def main():
     counts = list(map(int, input().split()))
     assert len(counts) == n_tables
     db = Database(counts)
-    for i in range(n_queries):
+    for _ in range(n_queries):
         dst, src = map(int, input().split())
         db.merge(dst - 1, src - 1)
         print(db.max_row_count)
