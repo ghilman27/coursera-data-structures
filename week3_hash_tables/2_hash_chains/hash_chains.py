35092d00c88d753b602a992bea5c20d8cdf55e0f
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-23 10:59:01 +0700

    week 3 problem 2 submit

--- week3_hash_tables/2_hash_chains/hash_chains.py
+++ week3_hash_tables/2_hash_chains/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[] for _ in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -37,25 +37,29 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(cur for cur in reversed(self.elems[query.ind]))
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+            ind = self._hash_func(query.s)
+            was_found = False
+            inner_ind = -1
+            for cur in reversed(self.elems[ind]):
+                if cur == query.s:
+                    was_found = True
+                    inner_ind = self.elems[ind].index(query.s)
+                    break
+            
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                self.write_search_result(was_found)
             elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+                if not was_found:
+                    self.elems[ind].append(query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                if inner_ind != -1:
+                    self.elems[ind].pop(inner_ind)
 
     def process_queries(self):
         n = int(input())
-        for i in range(n):
+        for _ in range(n):
             self.process_query(self.read_query())
 
 if __name__ == '__main__':
