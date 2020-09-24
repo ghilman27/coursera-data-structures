e28c74584160a7cb77d9606201bfee56e0cb48f4
ghilman27@gmail.com <ghilman27@gmail.com>
2020-09-24 11:20:59 +0700

    week 4: problem 2 submit

--- week4_binary_search_trees/2_is_bst/is_bst.py
+++ week4_binary_search_trees/2_is_bst/is_bst.py
@@ -1,20 +1,51 @@
 #!/usr/bin/python3
 
 import sys, threading
+from collections import namedtuple
 
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
+Node = namedtuple('Node', 'key left right')
 
 def IsBinarySearchTree(tree):
   # Implement correct algorithm here
+  if not tree:
+    return True
+  
+  crnt_node = tree[0]
+  prev_val = -sys.maxsize
+  stack = []
+
+  while True:
+    if crnt_node is not None:
+      stack.append(crnt_node)
+      if crnt_node.left != -1:
+        crnt_node = tree[crnt_node.left]
+      else:
+        crnt_node = None
+    
+    elif stack:
+      crnt_node = stack.pop()
+      if prev_val > crnt_node.key:
+        return False
+      prev_val = crnt_node.key
+      if crnt_node.right != -1:
+        crnt_node = tree[crnt_node.right]
+      else:
+        crnt_node = None
+    
+    else:
+      break
+  
   return True
 
 
 def main():
   nodes = int(sys.stdin.readline().strip())
   tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
+  for _ in range(nodes):
+    [a, b, c] = map(int, sys.stdin.readline().strip().split())
+    tree.append(Node(a, b, c))
   if IsBinarySearchTree(tree):
     print("CORRECT")
   else:
